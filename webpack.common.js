584b3890a3fa256f5315c331bb958a645695e5cd
Punith1117 <punith11172006@gmail.com>
2024-10-13 16:15:08 +0530

    add all required files with configuration

---
+++ webpack.common.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+entry: {
+    app: './src/index.js',
+},
+plugins: [
+    new HtmlWebpackPlugin({
+    title: 'Production',
+    template: './src/template.html',
+    filename: 'index.html'
+    }),
+],
+output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+},
+};
