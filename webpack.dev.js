64d34b9dc654c4f954483139a1449d3da511376a
Punith1117 <punith11172006@gmail.com>
2024-10-24 18:41:19 +0530

    module rules should be there in common.js not dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -8,22 +8,4 @@ module.exports = merge(common, {
     static: "./dist",
     watchFiles: ["./src/template.html"],
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-
-      {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
-
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-    ],
-  },
 });
